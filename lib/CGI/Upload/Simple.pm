e7a379e176c2e769d0a7c0c7fb112d40931c0e3c
xaicron <xaicron@gmail.com>
2009-06-18 22:58:44 +0900

    initial commit

---
+++ lib/CGI/Upload/Simple.pm
@@ -0,0 +1,33 @@
+package CGI::Upload::Simple;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+CGI::Upload::Simple -
+
+=head1 SYNOPSIS
+
+  use CGI::Upload::Simple;
+
+=head1 DESCRIPTION
+
+CGI::Upload::Simple is
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
